0b665e6eba35dfa1b36a50bf333e9e69f587d366
CameCome <dim.dimof3@gmail.com>
2026-02-11 21:35:39 +0300

    True initial commit

---
+++ kannada_mnist/test.py
@@ -0,0 +1,10 @@
+import fire
+
+
+def test():
+    # Yet to implement the testing loop for the model, so for now just a placeholder function
+    pass
+
+
+if __name__ == "__main__":
+    fire.Fire(test)
